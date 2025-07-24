b6be6383f5eaad54e3091a84ecf281676fd53294
matcir <matteocirilloboolean@gmail.com>
2025-07-24 19:11:44 +0200

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import DefaultLayout from "./layouts/DefaultLayout"
-import HomePage from "./pages/Homepage"
+import HomePage from "./pages/HomePage"
 import AboutUsPage from "./pages/AboutUsPage"
 import ProductsPage from "./pages/ProductsPage"
 
